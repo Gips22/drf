e14b835108a240c38b3e14f2e17abc440a78dee3
andrey <gipsme123@gmail.com>
2022-11-28 16:44:52 +0300

    добавил общую пагинацию и пагинацию только для класса FootballPlayersAPIList

--- football_players/views.py
+++ football_players/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
@@ -12,21 +13,30 @@ from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from .serializers import FootballPlayersSerializer
 
 
+class FootballPlayersAPIListPagination(PageNumberPagination):  # класс пагинации только для FootballPlayersAPIList
+    page_size = 3
+    page_size_query_param = 'page_size'  # дополнительный параметр для ручного изменения числа записей-пишется в url ...&page_size=4 и будет 4 записи, а не 3.
+    max_page_size = 10000
+
+
 class FootballPlayersAPIList(generics.ListCreateAPIView):
     queryset = FootballPlayers.objects.all()
     serializer_class = FootballPlayersSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = FootballPlayersAPIListPagination
+
 
 class FootballPlayersAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = FootballPlayers.objects.all()
     serializer_class = FootballPlayersSerializer
-    permission_classes = (IsAuthenticated, )  # просматривать могут только авторизованные пользователи
+    permission_classes = (IsAuthenticated,)  # просматривать могут только авторизованные пользователи
     # authentication_classes = (TokenAuthentication, )  # аутентификация по токенам только для этого view
+
 
 class FootballPlayerAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = FootballPlayers.objects.all()
     serializer_class = FootballPlayersSerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly,)
 
 ######
 # Общий класс для обработки всех роутов без использования permissions
